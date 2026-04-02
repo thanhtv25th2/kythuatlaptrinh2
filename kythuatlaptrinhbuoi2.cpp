fe2cee4ff4fbec0d142466a1d3dfb2a3a2f9a9c7
thanhtv25th2 <thanhtv.25th2@sv.dla.edu.vn>
2026-04-02 14:52:56 +0700

    Add files via upload

--- kythuatlaptrinhbuoi2.cpp
+++ kythuatlaptrinhbuoi2.cpp
@@ -1,20 +1,100 @@
-// kythuatlaptrinhbuoi2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
 
+struct Person
+{
+	int id, age;
+	string name, hometown;
+	friend istream& operator>>(istream& in, Person& p) {
+		cout << "Enter person information: " << endl;
+		cout << "\t+ Id: ";
+		in >> p.id;
+		cout << "\t + Name: ";
+		in.ignore();
+		getline(in, p.name);
+		cout << "\t+ Age: ";
+		in >> p.age;
+		cout << "\t+ Home town: ";
+		in.ignore();
+		getline(in, p.hometown);
+		return in;
+	}
+};
+void Show(vector<Person> p) {
+	if (p.size() == 0)
+		cout << "an empty list" << endl;
+	else {
+		for (int i = p.size() - 1; i <= 0; i++) {
+			cout << "Person information ";
+			cout << "\t+ Id: " << p[i].id << endl;
+			cout << "\t+ Name: " << p[i].name << endl;
+			cout << "\t+ Age: " << p[i].age << endl;
+			cout << "\t+ Home town" << p[i].hometown << endl;
+		}
+	}
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+	vector<Person> list;
+	do {
+		system("cls");
+		cout << "---- HUMAN RESOURCE MANAGEMENT ----" << endl;
+		cout << "1. Show person list " << endl;
+		cout << "2. Add a person " << endl;
+		cout << "3. Remove a person by id " << endl;
+		cout << "4. Find people by name " << endl;
+		cout << "5. Export to find " << endl;
+		cout << "6. Import from file " << endl;
+		cout << "0. Quit " << endl;
+		cout << "-----------------------------------" << endl;
+		cout << "Your command : ";
+		int cmd;
+		cin >> cmd;
+		switch (cmd) {
+		case 1: {
+			Show(list);
+			break;
+		}
+		case 2: {
+			Person a;
+			cout << "Id: ";
+			cin >> a.id;
+			cout << "Name: ";
+			cin >> a.name;
+			cout << "Age: ";
+			cin >> a.age;
+			cout << "Hometown: ";
+			cin >> a.hometown;
+			list.push_back(a);
+			break;
+		}
+		case 3: {
+			break;
+		}
+		case 4: {
+			break;
+		}
+		case 5: {
+			break;
+		}
+		case 6: {
+			break;
+		}
+		case 7: {
+			break;
+		}
+		case 0: {
+			return 0;
+		}
+		default: {
+			cout << "Your command isn't found. Try again" << endl;
+		}
+		}
+		cout << "Press Enter to continue. . .";
+		cin.ignore();
+		cin.get();
+	} while (true);
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
